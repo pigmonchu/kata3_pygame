388360d4f0f611e53df3acbce389657e60120fa8
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2019-07-02 00:35:57 +0200

    Fin de clase con pong

--- main.py
+++ main.py
@@ -5,16 +5,45 @@ import random
 
 _fps = 60
 
+class Raquet(pg.Surface):
+    x = 0
+    y = 0
+    w = 16
+    h = 96
+    color = (255, 255, 255)
+    velocidad = 15
+    diry = 1
+
+    def __init__(self):
+        pg.Surface.__init__(self, (self.w, self.h))
+        self.fill(self.color)
+
+    def setColor(self, color):
+        self.color = color
+        self.fill(self.color)
+
+    def avanza(self):
+        self.y += self.diry * self.velocidad
+
+        if self.y <=0:
+            self.y = 0
+ 
+        if self.y >= 600 - self.h:
+            self.y = 600 - self.h
+
+
 class Ball(pg.Surface):
     x = 0
     y = 0
+    w = 16
+    h = 16
     color = (255, 255, 255)
     velocidad = 5
     dirx = velocidad
     diry = velocidad
 
     def __init__(self):
-        pg.Surface.__init__(self, (16,16))
+        pg.Surface.__init__(self, (self.w, self.h))
         self.fill(self.color)
 
     def setColor(self, color):
@@ -34,6 +63,17 @@ class Ball(pg.Surface):
         self.x += self.dirx
         self.y += self.diry
 
+    def comprobarChoque(self, candidata):
+
+        if (candidata.x >= self.x and candidata.x <= self.x+self.w or \
+           candidata.x+candidata.w >= self.x and candidata.x+candidata.w <= self.x+self.w) and \
+           (candidata.y >= self.y and candidata.y <= self.y+self.h or \
+            candidata.y+candidata.h >= self.y and candidata.y+candidata.h <= self.y+self.h):
+
+            self.dirx = self.dirx * -1
+            self.x += self.dirx
+
+
 class Game:
     clock = pg.time.Clock()
 
@@ -44,17 +84,15 @@ class Game:
         self.screen.fill((60, 60, 60))
         self.display.set_caption('Mi juego')
 
-        self.balls = []
+        self.ball1 = Ball()
+        self.ball1.setColor((255, 0, 0))
+        self.ball1.x = random.randrange(800)
+        self.ball1.y = random.randrange(600)
+        self.ball1.velocidad = random.randrange(2, 9)
 
-        for i in range(random.randrange(5,15)):
-            b = Ball()
-            b.setColor((random.randrange(256), random.randrange(256), random.randrange(256)))
-            b.x = random.randrange(800)
-            b.y = random.randrange(600)
-            b.velocidad = random.randrange(10)
-
-            self.balls.append(b)
-
+        self.player1 = Raquet()
+        self.player1.x = 768
+        self.player1.y = 252
 
     def start(self):
         while True:
@@ -65,15 +103,25 @@ class Game:
                     pg.quit()
                     sys.exit()
 
+                if event.type == KEYDOWN:
+                    if event.key == K_UP:
+                        self.player1.diry = -1
+                        self.player1.avanza()
+
+                    if event.key == K_DOWN:
+                        self.player1.diry = 1
+                        self.player1.avanza()                    
+
             #Modifica la posición de ball
-            for ball in self.balls:
-                ball.avanza()
+            self.ball1.avanza()
+            self.ball1.comprobarChoque(self.player1)
+
 
             #Pintar los sprites en screen
             self.screen.fill((60,60,60))
 
-            for lolailo in self.balls:
-                self.screen.blit(lolailo, (lolailo.x, lolailo.y))
+            self.screen.blit(self.ball1, (self.ball1.x, self.ball1.y))
+            self.screen.blit(self.player1, (self.player1.x, self.player1.y))
 
             self.display.flip()
 
